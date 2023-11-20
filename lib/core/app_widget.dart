681f4b60658b41cddc4bbeea42215ac3fe60645e
Treeunfe FrontEnd 2 <treeunfe-frontend-2@pop-os.localdomain>
2023-11-20 17:30:12 -0300

    Configuração inicial

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../presentation/presentation.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather now',
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)),
+      home: const SplashPage(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,6 @@
-import 'package:consultar_clima/page/pag1.dart';
-import 'package:consultar_clima/page/theme/temas.dart';
 import 'package:flutter/material.dart';
+import 'core/core.dart';
 
-final tema = ValueNotifier(ThemeMode.light);
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-        valueListenable: tema,
-        builder: (BuildContext context, dynamic value, Widget? child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            themeMode: value,
-            theme: ThemeData(
-              useMaterial3: true,
-              colorScheme: lightColorScheme,
-            ),
-            darkTheme: ThemeData(
-              useMaterial3: true,
-              colorScheme: darkColorScheme,
-            ),
-            home: const Pag1(),
-          );
-        });
-  }
+  runApp(const AppWidget());
 }
