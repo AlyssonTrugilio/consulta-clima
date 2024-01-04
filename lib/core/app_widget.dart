c2e8ed4608b56832d50b47112c1ef4e00e02b74b
AlyssonTrugilio <alyson.silvatrugilio121@gmail.com>
2024-01-04 15:30:37 -0300

    style: Alterando a cor principal para amber

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -14,7 +14,7 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.blue, brightness: Brightness.dark)),
+              seedColor: Colors.amber, brightness: Brightness.dark)),
       initialRoute: '/splash',
       onGenerateRoute: (setting) {
         switch (setting.name) {
